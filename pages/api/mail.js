f181013af1d6ebbfc57ef820170c445a767356b4
Julian <julian.costinea@gmail.com>
2021-08-13 20:33:09 +0200

    integrated sendgrid with api/mail

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,7 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { resolveHref } from "next/dist/next-server/lib/router/router";
 
-export default function handler(req, res) {
+const mail = require("@sendgrid/mail");
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default async function handler(req, res) {
   const body = JSON.parse(req.body);
-  console.log(body.name);
-  res.status(200).json({ status: 'Ok' })
+
+  const message = `
+    Navn: ${body.name} \r\n
+    Mail: ${body.email} \r\n
+    Telefonnummer: ${body.telefon} \r\n
+    Besked: ${body.description} \r\n
+  `;
+
+  try {
+    await mail.send({
+      to: "job@go-work.dk",
+      from: "job@go-work.dk",
+      subject: `Mail sendt gennem hjemmesiden. Afsender: ${body.email}`,
+      text: message,
+      html: message.replace(/\r\n/g, "<br>"),
+    });
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(process.env.SENDGRID_API_KEY);
+    console.log(error);
+    console.log("Error sending the email");
+  }
 }
