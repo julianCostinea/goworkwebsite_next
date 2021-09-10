fa2087ad270aacc0ab6c4fea0363a346bd036139
julian <julian.costinea@gmail.com>
2021-09-10 15:11:21 +0200

    added google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 import '../styles/globals.css'
 import Layout from '../hoc/Layout/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Layout>
-    <Head> 
-    <link rel="icon" href="/logoIcon.png" />
-    </Head>
-    <Component {...pageProps} />
-  </Layout>
+    <Layout>
+      <Head>
+        <link rel="icon" href="/logoIcon.png" />
+      </Head>
+      <Component {...pageProps} />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1WZEXKXJ0K"></Script>
+      <Script type="text/javascript" src="/js/analytics.js"></Script>
+    </Layout>
   );
 }
 
