8b4e941ec7a5e03aed06ffae24905bf92f001119
julian <julian.costinea@gmail.com>
2021-10-06 13:27:45 +0200

    working with style on cookie declaration

--- pages/cookieDeclaration/index.js
+++ pages/cookieDeclaration/index.js
@@ -1,11 +1,12 @@
 import Script from 'next/script';
+import Layout from '../../hoc/Layout/Layout';
 
 function CookieDeclaration({ Component, pageProps }) {
     return (
-
-            <Script id="CookieDeclaration" src="https://consent.cookiebot.com/2f7dcd32-8bd8-433e-bb6f-03dadafb2ad9/cd.js" type="text/javascript" async></Script>
-
-    );
+        <Layout>
+            <Script id="CookieDeclaration" src="https://consent.cookiebot.com/2f7dcd32-8bd8-433e-bb6f-03dadafb2ad9/cd.js" type="text/javascript" async></Script>      <Script type="text/javascript" src="/js/analytics.js"></Script>
+        </Layout>
+    )
 }
 
 export default CookieDeclaration
