7e0f586e126c224915ed360af6ce1b5819de6659
julian <julian.costinea@gmail.com>
2021-10-06 13:33:58 +0200

    working with style on cookie declaration v2

--- pages/cookiepolicy/index.js
+++ pages/cookiepolicy/index.js
@@ -4,7 +4,9 @@ import Layout from '../../hoc/Layout/Layout';
 function CookieDeclaration({ Component, pageProps }) {
     return (
         <Layout>
+            <div style={{maxWidth: "80vw"}}>
             <Script id="CookieDeclaration" src="https://consent.cookiebot.com/2f7dcd32-8bd8-433e-bb6f-03dadafb2ad9/cd.js" type="text/javascript" async></Script>      <Script type="text/javascript" src="/js/analytics.js"></Script>
+            </div>
         </Layout>
     )
 }
