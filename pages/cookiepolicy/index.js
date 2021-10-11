afc1ad93c5e7a98916a2bee6a4d52257b36e81fc
julian <julian.costinea@gmail.com>
2021-10-11 12:25:52 +0200

    added styling to cookiepolicy

--- pages/cookiepolicy/index.js
+++ pages/cookiepolicy/index.js
@@ -1,10 +1,12 @@
-import classes from './cookiepolicy.module.css';
+import Script from 'next/script';
+import React from 'react';
 
 function CookieDeclaration() {
     return (
-        <div className={classes.Content}>
-            <iframe id={'cookiePolicy'} src="/cookiePolicy.html" width="100%" height="1000px"></iframe>
-        </div>
+        <React.Fragment>
+            <link rel="stylesheet" href="cookiepolicy.css"></link>
+            <Script id="CookieDeclaration" src="https://consent.cookiebot.com/2f7dcd32-8bd8-433e-bb6f-03dadafb2ad9/cd.js" type="text/javascript" async></Script>
+        </React.Fragment>
     )
 }
 
