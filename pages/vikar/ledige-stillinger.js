cf3e1980e167baa9a4827df403525a853b5dabce
julian <julian.costinea@gmail.com>
2021-11-01 18:04:00 +0100

    changed resizing on list jobs v2

--- pages/vikar/ledige-stillinger.js
+++ pages/vikar/ledige-stillinger.js
@@ -4,22 +4,24 @@ function LedigeStillinger() {
     const adjustHeight = () => {
         const iframe = document.querySelector('#HRon');
         const iframeHeight = iframe.contentWindow.document.body.scrollHeight;
-        setTimeout(function () {
-            if (iframeHeight < 1000) {
-                adjustHeight();
-            } else if (iframeHeight > 1000) {
-                console.log("LOADED 1");
-                console.log(iframeHeight);
-                iframe.height = iframeHeight;
-                return;
-            }
-            else {
-                console.log("LOADED 2");
-                console.log(iframeHeight);
-                iframe.height = iframeHeight;
-                return;
-            }
-        }, 100);
+        // setTimeout(function () {
+        //     if (iframeHeight < 1000) {
+        //         adjustHeight();
+        //     } else if (iframeHeight > 1000) {
+        //         console.log("LOADED 1");
+        //         console.log(iframe.contentWindow.document.body.scrollHeight);
+        //         iframe.height = iframeHeight;
+        //         return;
+        //     }
+        //     else {
+        //         console.log("LOADED 2");
+        //         console.log(iframe.contentWindow.document.body.scrollHeight);
+        //         iframe.height = iframeHeight;
+        //         return;
+        //     }
+        // }, 100);
+        console.log(iframeHeight);
+        console.log(document.querySelector('#HRon').contentWindow.document.body.scrollHeight);
     }
 
     return (
