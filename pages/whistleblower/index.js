7dec2fd92b1b91b9009b229ed96170fc17b9e46a
julian <julian.costinea@gmail.com>
2021-12-17 12:35:33 +0100

    added content to whistleblower

--- pages/whistleblower/index.js
+++ pages/whistleblower/index.js
@@ -6,7 +6,65 @@ const Whistleblower = () => {
     return (
         <React.Fragment>
             <div className={classes.Content}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque urna id erat posuere eleifend. Mauris urna libero, consequat et sem ut, feugiat faucibus diam. In quis massa vitae urna laoreet bibendum. Aliquam erat volutpat. Aliquam porta sollicitudin nisi. Donec neque lacus, laoreet sit amet facilisis rhoncus, aliquet sit amet lorem. Integer eleifend eros a feugiat mollis. Aenean in velit feugiat, iaculis nulla quis, vulputate urna. Donec sed erat semper, congue diam non, sollicitudin sem. Suspendisse a sollicitudin tellus. Maecenas egestas eleifend turpis quis bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas ut metus ac quam pulvinar rhoncus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam et interdum eros.
+                <h1>GoWorks Whistleblowerordning</h1>
+
+                <h2>Typiske forhold, der kan indberettes</h2>
+                <p>Formålet med denne whistleblowerordning er at skabe muligheden for fortroligt at indberette kriminelle eller andre alvorlige forhold vedr. GoWork. </p>
+                <p>De typer af forhold som kan indberettes til Whistleblowerordningen er som følger: </p>
+                <ul>
+                    <li>Økonomisk kriminalitet som fx overfakturering, svindel, bedrageri, tyveri eller lign.</li>
+                    <li>Afgivelse af urigtige eller vildledende oplysninger til offentlige myndigheder.</li>
+                    <li>Fysisk vold eller seksuelle krænkelser.</li>
+                    <li>Forhold omkring alvorlige brud på arbejdssikkerheden.</li>
+                    <li>Forhold, som kan have betydning for enkeltpersoners liv eller helbred.</li>
+                    <li>Brud på GDPR. </li>
+                    <li>Generelt kriminelle forhold, som overtræder straffeloven. </li>
+                </ul>
+                <p>Det skal bemærkes, at man i GoWorks Whistleblowerordning kun kan indberettes forhold vedr. GoWork. Hvis det er vedr. forhold ved en virksomhed, hvor du er eller har været vikar, så skal indberetningen ske via den pågældende virksomheds egen whistleblowerordning. </p>
+
+                <h2>Hvilke forhold hører ikke under whistleblowerordningen</h2>
+                <p>Det er bl.a. forhold som disse: </p>
+                <ul>
+                    <li>Klager fra kunder. Disse modtages direkte hos din almindelige kontaktperson hos GoWork</li>
+                    <li>Inkompetence</li>
+                    <li>Fravær</li>
+                    <li>Samarbejdsvanskeligheder</li>
+                    <li>Mobning</li>
+                    <li>Overtrædelser af retningslinjer for fx påklædning, alkohol, brug af internet og lignende. </li>
+                </ul>
+                <p>Ved sådanne forhold direkte til nærmeste leder eller dennes leder. Forhold som disse må ikke behandles af whistleblowerenheden – de vil derfor blive slette med det samme. </p>
+
+                <h2>Hvem kan indberette</h2>
+                <p>Denne whistleblowerordning er lavet for, at medarbejdere, samarbejdspartnere og leverandører sikkert og fortroligt kan indberette jf. de ovenstående rammer. Hvis du falder indenfor en af disse grupper, kan du derfor indberette via de nedenstående kanaler. </p>
+
+                <h2>Hvordan indberetter man</h2>
+                <p>Der er to muligheder for indberetning. </p>
+                <p>1. Via e-mail to whistleblower@go-work.dk</p>
+                <p style={{ marginLeft: "3rem" }}>a.	Hvis din indberetning indeholder følsomme eller fortrolige personoplysninger, skal henvendelsen ske på en sikker måde, dvs. at du ikke bør sende indberetningen med almindelig mail, men fx sikre dig, at mailen bliver sendt krypteret.</p>
+                <p>2. Via fysisk brev til GoWork på Fjordgade 11, 5000 Odense C. (postkassen er lige inden for døren til opgangen, hvor kontoret ligger). Skriv på brevet ”Må ikke åbnes – afleveres til whistleblowerenheden”. </p>
+
+                <p>I din indberetning påfør da gerne følgende, hvis muligt:</p>
+                <ul>
+                    <li>Hvad drejer indberetningen sig om,</li>
+                    <li>Hvem der er involveret i den pågældende handling,</li>
+                    <li>Hvor den pågældende handling foregår eller har foregået,</li>
+                    <li>Hvordan du er kommet i besiddelse af dine oplysninger.</li>
+                </ul>
+                <p>Det er dog vigtigt, at du ikke selv begynder at undersøge sagen nærmere, hvis du ikke har svar på alle de ovenstående punkter. Så er det bedre med en indberetning på mindre sikkert grundlag, end at der foretages handlinger, som potentielt kan føre til ødelæggelse af eventuelle beviser eller lign. </p>
+                
+                <h2>Beskyttelse af Whistleblower</h2>
+                <p>Whistleblowere, som gør brug af denne ordning, samt alle involverede parter, er sikret fortrolighed og beskyttes af GoWork. Din indberetning vil derfor blive håndteret af en upartisk whistleblowerenhed. Hvis der oplyses kontaktoplysninger i indberetningen, vil disse kun være tilgængelige for whistleblowerenheden. </p>
+                <p>Der kan være særlige forhold, som gør, at vi er nødt til at oplyse whistleblowerens identitet overfor en tredjepart. Det kan fx være ved anmeldelse til politiet.</p>
+                <p>Ønsker whistlebloweren at være anonym skal der vælges en indberetningsform, som sikrer, at vi ikke oplyses om whistleblowerens identitet. Vær derfor opmærksom på ikke utilsigtet at afgive information på dig selv, som whistleblower. </p>
+                <p>Hvis whistlebloweren vælger at være anonym, skal man være opmærksom på, at det kan være vanskeligt for whistleblowerenheden at undersøge sagen, da der ikke kan stilles opfølgende eller opklarende spørgsmål til whistlebloweren. Derudover kan muligheden for feedback til whistlebloweren frafalde.</p>
+                <p>Som whistleblower er du beskyttet mod at blive udsat for repressalier som følge af en indberetning. Opleves der repressalier indberettes dette til GoWorks ledelse.</p>
+            
+                <h2>Hvordan behandles en indberetning</h2>
+                <p>Ved modtagelse af en indberetning vil der sendes bekræftelse på modtagelse indenfor 7 dage – i de tilfælde, hvor der er muligt at kontakte whistlebloweren. </p>
+                <p>Whistleblowerenheden vil vurdere, som indberetningen falder indenfor rammerne for whistleblowerordningen. </p>
+                <p>Vurderes det, at indberetningen falder udenfor whistleblowerordningens rammer, vil den blive slettet omgående, da den derved ikke må behandles af enheden. Hvis muligt og relevant vil whistleblowerenheden kort vejlede om den korrekte kanal til indberetning af den pågældende sag ifm. sletning. </p>
+                <p>Hvis indberetningen falder indenfor ordningens rammer, vil vi behandle den hurtigst muligt. Ved juridiske spørgsmål vil GoWorks juridiske samarbejdspartnere blive inddraget. Derudover kan der også være tale om inddragelse af eksterne myndigheder (fx politiet) og lign., hvis det findes relevant af enheden. </p>
+                <p>Whistlebloweren vil modtage feedback på indberetningen indenfor tre måneder fra indberetningen er bekræftet modtaget. </p>
             </div>
         </React.Fragment>
     )
