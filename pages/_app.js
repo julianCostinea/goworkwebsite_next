0971139dc5a7ba2d8c71d5a5b39474d07c70b8a5
julian <julian.costinea@gmail.com>
2022-01-20 14:33:16 +0100

    updated FB pixel script

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,8 @@ function MyApp({ Component, pageProps }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ${fbq.FB_PIXEL_ID});
+            fbq('init', '3003946606526491');
+            fbq('track', 'PageView');
           `,
         }}
       />
--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class Dokument extends Document {
                             height="1"
                             width="1"
                             style={{ display: 'none' }}
-                            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                            src={`https://www.facebook.com/tr?id=3003946606526491&ev=PageView&noscript=1`}
                         />
                     </noscript>
                 </ Head >
